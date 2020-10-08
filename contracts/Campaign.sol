16ee650f6fe56cc6feb6de5dc3d534c91a24deb4
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2020-10-08 22:22:07 +0800

    finalizing the  request

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -3,17 +3,18 @@ pragma solidity ^0.4.17;
 contract Campaign {
     struct Request {
         mapping(address => bool) approvals;
-        string description;
-        uint value;
-        address recipient;
-        bool complete;
         uint approvalCount;
+        string description;
+        address recipient;
+        uint value;
+        bool complete;
     }
     
     address public manager;
-    uint public minimumContribution;
-    mapping(address => bool) public approvers;
     Request[] public requests;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+    uint public minimumContribution;
     
     modifier restricted() {
         require(msg.sender == manager);
@@ -28,6 +29,7 @@ contract Campaign {
     function Contribute() public payable {
         require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
+        approversCount++;
     }
     
     function createRequest(string description, uint value, address recipient) public restricted {
@@ -50,14 +52,14 @@ contract Campaign {
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        
+        require(!request.complete);
+        require(request.approvalCount > (approversCount / 2));
+        
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
 }
-
-
-
-
-
-
-
-
-
-

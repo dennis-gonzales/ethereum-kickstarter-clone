836a91e999be3417d0377c0f0bae3bd102079913
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2020-12-06 20:26:08 +0800

    added body parser

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const next = require('next');
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-  express().use(handler).listen(3000, (err) => {
+  const server = express();
+  
+  server.use(bodyParser.urlencoded({ extended: true }))
+  server.use(bodyParser.json())
+  server.use(handler).listen(3000, (err) => {
         if (err) throw err;
         else console.log('started server on http://localhost:3000');
     })
